50ed7e1b66a3964f8b06381f36e14318ef4a1726
Prakash Gurung <prakashgrg4400@gmail.com>
2025-07-27 01:40:48 +0545

    feat: add login basic form

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,38 +1,16 @@
 import axios from "axios";
-import ENV from "@/config";
-import toast from "react-hot-toast";
+import  ENV  from "@/config";
 
 const apiClient = axios.create({
-    baseURL: `${ENV.BASE_URL}/api`,
+  baseURL: `${ENV.BASE_URL}/api`,
 });
 
 apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const status = error?.response?.status;
-    const detail = error?.response?.data?.detail;
-
-    const isAuthError =
-      status === 401 || // Unauthorized
-      (status === 400 && detail === "Signature verification failed");
-
-    if (isAuthError) {
-      toast.error(detail || "Session expired. Please login again.");
-      localStorage.removeItem("token");
-      window.location.href = "/login";
-    }
-
-    return Promise.reject(error);
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
-
+  return config;
+});
 
 export default apiClient;
