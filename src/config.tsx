8a270b97e3f5395accdfda48d1fd34c3481cba49
kall37 <rimalsubash77@gmail.com>
2025-10-31 17:58:01 +0545

    refactor: change the link

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,6 @@
 const ENV = {
     // BASE_URL: "http://localhost:8000"
     BASE_URL:
-        "https://presentations-miller-bureau-nottingham.trycloudflare.com",
+        "https://hearings-olive-record-rangers.trycloudflare.com",
 };
 export default ENV;
